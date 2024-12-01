7da5512c6eff8fdac7647ac20b3fc7c22fe0b1db
MD. RAHAT ALI <merahatbd@gmail.com>
2024-12-01 13:09:10 +0600

    Add files via upload

--- PHP/28-11-2024/up.php
+++ PHP/28-11-2024/up.php
@@ -1,33 +1,50 @@
 <?php
+if (isset($_POST["submit"])) {
 
-    if(isset($_POST["btnSubmit"]))
-    {
-        $file = $_FILES["file"];
-        $fileName = $file["name"];
-        $fileTmpName = $file["tmp_name"];
-        $fileSize = $file["size"];
-       
-        $img = "image/";
+    $filename = $_FILES["my-file"]["name"]; //file name
+    $temp = $_FILES["my-file"]["tmp_name"];
+
+    $file_size = $_FILES["my-file"]["size"];
+    $kb = 102400;
+
+
+    if ($file_size > $kb) {
+        $img ="image/";
+        echo "file is tooo large";
+
+        
+    } else {
+        $img ="image/";
+        move_uploaded_file("$temp","$img.$filename");
+        echo "done";
     }
-
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
+
     <form action="#" method="post" enctype="multipart/form-data">
-        <label for="file">Select File:</label>
-        <input type="file" id="file" name="file" accept=".jpg,.png,.gif">
-        <input type="submit" value="Upload" name="btnSubmit">
+
+        <input type="file" name="my-file">
+        <input type="submit" value="submit" name="submit"><br>
     </form>
 
     <?php
-    echo  "<img src='$img.$file' alt=''>";
-    ?>
     
+    echo "<img src='$img.$filename'>";
+
+
+
+    ?>
+
 </body>
+
 </html>
